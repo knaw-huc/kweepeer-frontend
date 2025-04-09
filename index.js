7adb252538a1116cfcc2099010ce3563bfb3816a
Maarten van Gompel <proycon@anaproy.nl>
2025-04-09 15:49:10 +0200

    implemented select all/none buttons

--- index.js
+++ index.js
@@ -5,9 +5,3 @@ const app = () => {
 }
 
 document.addEventListener('DOMContentLoaded', app);
-
-function expand() {
-    const kweeper = document.getElementById("kweepeer");
-    const query = document.getElementById("q");
-    kweepeer.query(query.value);
-}
--- kweepeer-frontend.js
+++ kweepeer-frontend.js
@@ -40,44 +40,62 @@ class QueryExpansionPanel extends HTMLElement {
     }
 
     update() {
-        var s = "<h4>Modules</h4> <ul class=\"modules\">";
+        var s = "<h4>Modules</h4>\n<ul class=\"modules\">\n";
         this.modules.forEach(module => {
             if ((this.include.length === 0 || this.include.includes(module.id)) &&
                 (this.exclude.length === 0 || !this.exclude.includes(module.id))) {
-                s += "<li>" + module_header(module, true) + "</li>";
+                s += "<li>" + module_header(module, true) + "</li>\n";
             }
         });
-        s += "</ul>";
+        s += "</ul>\n";
         if (this.response !== null) {
-            s += "<h4>Term Expansions</h4>";
+            s += "<h4>Term Expansions</h4>\n";
             var termindex = 0;
             for (const [term, sources] of Object.entries(this.response.terms)) {
-                s += "<ol class=\"terms\">";
-                s += "<li><span>" + term + "</span>";
-                s += "<ul class=\"sources\">";
+                s += `<ol class="terms">
+                         <li><span>${term}</span>
+                         <ul class="sources">
+                `;
                 var expansionindex = 0;
                 sources.forEach(source => {
-                    s += "<li><span>" + source.source_name + "</span>";
-                    s += "<ul class=\"expansions\">";
+                    var s2 = "";
+                    var beginindex = expansionindex;
                     source.expansions.forEach(expansion => {
-                        var id="term" + termindex + "." + expansionindex;
-                        var attribs = " checked=\"checked\"";
-                        s += "<li> <input type=\"checkbox\" name=\"" + id + "\" id=\"" + id + "\" value=\"" + expansion + "\"" + attribs + "> <label for=\"" + id + "\">" + expansion + "</label></li>";
-                        expansionindex += 1;
+                        var id="term" + termindex + "-" + expansionindex;
+                        var attribs = ' checked="checked"';
+                        s2 += `<li> <input type="checkbox" name="${id}" id="${id}" value="${expansion}" ${attribs}> <label for="${id}">${expansion}</label></li>\n`;
+                        expansionindex++;
                     });
-                    s += "</ul></li>";
+                    s += `      <li><label>${source.source_name}</label>
+                                    <button data-source-id="${source.source_id}" data-term-index="${termindex}" data-beginterm="${beginindex}" data-endterm="${expansionindex}" class="selectall">+</button> 
+                                    <button data-source-id="${source.source_id}" data-term-index="${termindex}" data-beginterm="${beginindex}" data-endterm="${expansionindex}" class="selectnone">-</button> 
+                                    <ul class="expansions">
+                                    ${s2}
+                                    </ul>
+                                </li>
+                    `;
                 });
-                s += "</ul>";
-                s += "</li>";
-                s += "</ol>";
-                termindex += 1;
+                s += "    </ul>\n";
+                s += "  </li>\n";
+                s += "</ol>\n";
+                termindex++;
             };
         }
         this.render(s);
+
+
+        // implements select all / none buttons
+        document.querySelectorAll(".selectall").forEach((element) => {
+            element.onclick = selectall;
+        });
+        document.querySelectorAll(".selectnone").forEach((element) => {
+            element.onclick = selectnone;
+        });
     }
 
+
     render(s) {
-        this.innerHTML = "<div class=\"kweepeer\">" + s + "</div>";
+        this.innerHTML = "<div class=\"kweepeer\">\n" + s + "\n</div>\n";
     }
 
 
@@ -129,6 +147,24 @@ function module_header(module, checked) {
     if (checked === true) {
         attribs = " checked=\"checked\"";
     }
-    return "<input type=\"checkbox\" id=\"kweepeer-module-" + module.id + "\"" + attribs + "> <label>" + module.name + "</label> <span class=\"type\">(" + module.type + ")</span>";
+    return `<input type="checkbox" id="kweepeer-module-${module.id}" ${attribs}> <label>${module.name}</label> <span class="type">(${module.type})</span>`;
 }
 
+
+function selectall(event) {
+    var target = event.target;
+    console.log(`DEBUG selectall: ${target.dataset.termIndex} ${target.dataset.beginterm} ${target.dataset.endterm}`);
+    for (var i = target.dataset.beginterm; i < target.dataset.endterm; i++) {
+        var e = document.getElementById(`term${target.dataset.termIndex}-${i}`);
+        e.checked = true;
+    }
+}
+
+function selectnone(event) {
+    var target = event.target;
+    console.log(`DEBUG selectnone: ${target.dataset.termIndex} ${target.dataset.beginterm} ${target.dataset.endterm}`);
+    for (var i = target.dataset.beginterm; i < target.dataset.endterm; i++) {
+        var e = document.getElementById(`term${target.dataset.termIndex}-${i}`);
+        e.checked = false;
+    }
+}
